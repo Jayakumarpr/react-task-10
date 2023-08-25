4115bdef13c2b50eff29f2c4319a46670df5896a
Jayakumar pr <135139859+Jayakumarpr@users.noreply.github.com>
2023-08-26 01:33:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+import HandlePage from './components/HandlePage';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+ 
+     <div>
+
+     
+      <Navbar/>
+      <HandlePage/>
+      </div> 
+  );
+}
+
+export default App;
